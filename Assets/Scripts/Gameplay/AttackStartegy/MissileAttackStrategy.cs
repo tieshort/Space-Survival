db25c502a66dfecacabe9896f667fd7625e471d6
tieshort <razumoffgleb@gmail.com>
2026-04-16 22:02:53 +0300

    update: Переименовал класс для консистентности, убрал ненужную проверку таймера от прошлой реализации

--- Assets/Scripts/Gameplay/AttackStartegy/MissileAttackStrategy.cs
+++ Assets/Scripts/Gameplay/AttackStartegy/MissileAttackStrategy.cs
@@ -1,18 +1,13 @@
 using UnityEngine;
 
 //[CreateAssetMenu(fileName = "MissileShotAttackStrategy", menuName = "Scriptable Objects/Missile Shot Attack Strategy")]
-public class MissileShotAttackStrategy : AttackStrategy
+public class MissileAttackStrategy : AttackStrategy
 {
     public GameObject missilePrefab;
     public string missileLayerName;
 
     public override void Attack()
     {
-        if (timer > 0)
-        {
-            return;
-        }
-
         Vector2 actualDirection = CalculateActualDirection(transform.up);
 
         var missile = Instantiate(missilePrefab, attackOrigin.position, Quaternion.LookRotation(Vector3.forward, actualDirection));
