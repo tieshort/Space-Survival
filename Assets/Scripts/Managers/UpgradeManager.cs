3ddfb192e9acc3ba660cbee371f6ce2bd6f71050
tieshort <razumoffgleb@gmail.com>
2026-05-03 09:07:05 +0300

    Добавил загрузку улучшений через UpgradeManager

--- Assets/Scripts/Managers/UpgradeManager.cs
+++ Assets/Scripts/Managers/UpgradeManager.cs
@@ -11,11 +11,50 @@ public class UpgradeManager: MonoBehaviour
 
     private void Start()
     {
-        //LoadUpgrade(upgradeInstance);
+        LoadUpgrades();
     }
 
     public void LoadUpgrades()
     {
+        if (upgradeInstance == null) return;
 
+        LoadUpgrade(upgradableHealth);
+        LoadUpgrade(upgradableAttack);
+        LoadUpgrade(upgradableMovement);
+        LoadUpgrade(upgradableMovement);
+    }
+
+    public void LoadUpgrade(Health health)
+    {
+        if (health == null) return;
+
+        health.AdditionalBaseHealth = upgradeInstance.healthUpgrade.AdditionalBaseValue;
+        health.HealthMultiplier = upgradeInstance.healthUpgrade.ValueMultiplier;
+    }
+
+    public void LoadUpgrade(Attack attack)
+    {
+        if (attack == null) return;
+
+        attack.AdditionalBaseDamage = upgradeInstance.damageUpgrade.AdditionalBaseValue;
+        attack.DamageMultiplier = upgradeInstance.damageUpgrade.ValueMultiplier;
+
+        attack.AttackSpeedMultiplier = upgradeInstance.attackSpeedUpgrade.ValueMultiplier;
+    }
+
+    public void LoadUpgrade(Movement movement)
+    {
+        if (movement == null) return;
+
+        movement.AdditionalBaseSpeed = upgradeInstance.movementUpgrade.AdditionalBaseValue;
+        movement.SpeedMultiplier = upgradeInstance.movementUpgrade.ValueMultiplier;
+    }
+
+    public void LoadUpgrade(Rotation rotation)
+    {
+        if (rotation == null) return;
+
+        rotation.AdditionalBaseSpeed = upgradeInstance.rotationUpgrade.AdditionalBaseValue;
+        rotation.SpeedMultiplier = upgradeInstance.rotationUpgrade.ValueMultiplier;
     }
 }
